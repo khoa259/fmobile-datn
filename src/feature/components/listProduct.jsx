787daedaa8d02d95a2f119a9b09ca6e0ae82ac1f
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-09-28 12:13:09 +0700

    API-30: Fake api detail products

--- src/feature/components/listProduct.jsx
+++ src/feature/components/listProduct.jsx
@@ -6,8 +6,6 @@ import { useNavigate } from "react-router-dom";
 import { addToCart } from "../../slice/CartSlice";
 import { getProducts } from "../../slice/productSlice";
 
-
-
 const ListProduct = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -15,7 +13,6 @@ const ListProduct = () => {
   useEffect(() => {
     dispatch(getProducts())
       .unwrap()
-
       .then((value) => {
         setProducts(value);
       })
@@ -29,41 +26,37 @@ const ListProduct = () => {
 
   return (
     <div>
-      {products && (
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products &&
-              products?.map((prd, index) => (
-                <div className="group relative bg-gray-100 p-5" key={index}>
-                  <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                    <img
-                      src={prd?.img}
-                      alt="Front of men's Basic Tee in black."
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
-                  </div>
-                  <div className="mt-4 flex justify-between">
-                    <div>
-                      <h3 className="text-sm text-gray-700">
-                        {/* <a href="#"> */}
-                        <span aria-hidden="true" className="absolute" />
-                        {prd?.name}
-                        {/* </a> */}
-                      </h3>
-                      <p className="mt-1 text-sm text-gray-500">{prd?.color}</p>
-                    </div>
-                    <p className="text-sm font-medium text-gray-900">${prd?.price}</p>
-                  </div>
-                  <button className="bg-red-500 px-2 py-1 text-white " type="button" onClick={() => AddtoCart(prd)}>
-                    Add to card
-                  </button>
-
-  
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products &&
+            products?.map((prd, index) => (
+              <div className="group relative bg-gray-100 p-5" key={index}>
+                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src={prd?.img}
+                    alt="Front of men's Basic Tee in black."
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
                 </div>
-             
-          </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      {/* <a href="#"> */}
+                      <span aria-hidden="true" className="absolute" />
+                      {prd?.name}
+                      {/* </a> */}
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{prd?.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">${prd?.price}</p>
+                </div>
+                <button className="bg-red-500 px-2 py-1 text-white " type="button" onClick={() => AddtoCart(prd)}>
+                  Add to card
+                </button>
+              </div>
+            ))}
         </div>
-      )}
+      </div>
     </div>
   );
 };
